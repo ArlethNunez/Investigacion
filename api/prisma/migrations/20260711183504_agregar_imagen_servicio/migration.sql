d1d52dfa3d8291431976a74a99baea8faf063e1b
Flow301 <arayamejiassebastian@gmail.com>
2026-08-16 14:32:09 -0600

    Delete api directory

--- api/prisma/migrations/20260711183504_agregar_imagen_servicio/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `servicios` ADD COLUMN `imagen` VARCHAR(255) NULL;
--- api/src/services/common.service.ts
+++
@@ -1,7 +0,0 @@
-export function toDateOnly(value: string) {
-    return new Date(`${value}T00:00:00`);
-}
-
-export function toTime(value: string) {
-    return new Date(`1970-01-01T${value}:00`);
-}
